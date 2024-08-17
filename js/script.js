22ef9e3a41e187b5e44ab7c58384919eb0db3060
Saad Razzaq <96721960+Saad4123@users.noreply.github.com>
2024-08-17 06:01:30 -0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -171,21 +171,13 @@
 
 	// Counter
 
-	$('.counter-stat span').counterUp({
-	      delay: 10,
-	      time: 1000
-	  });
 
 		
  // Shuffle js filter and masonry
     var Shuffle = window.Shuffle;
     var jQuery = window.jQuery;
 
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-        itemSelector: '.shuffle-item',
-        buffer: 1
-    });
-
+ 
     jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
         var input = evt.currentTarget;
         if (input.checked) {
